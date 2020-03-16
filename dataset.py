e6ed048cf433d36dd75e75d8695c973884f0f004
Yannick Knapp <YaBaSa@web.de>
2020-03-16 14:35:44 +0000

    Replace variable KITTI with default mean calibration file and fix wrong audi stuff

--- dataset.py
+++ dataset.py
@@ -1,7 +1,7 @@
 import os
 import numpy as np
 from PIL import Image
-from calibration import KittiCalibration, AudiCalibration
+from calibration import Calibration, KittiCalibration, AudiCalibration
 import cv2
 
 
@@ -28,9 +28,35 @@ class Kitti:
         return Image.open(img_file).convert("L")
 
     def get_calib(self, idx):
-        calib_file = os.path.join(self.calib_path, self.files_list[idx].replace('.bin', '.txt'))
-        assert os.path.exists(calib_file)
-        return KittiCalibration(calib_file)
+        #calib_file = os.path.join(self.calib_path, self.files_list[idx].replace('.bin', '.txt'))
+        #assert os.path.exists(calib_file)
+        #return KittiCalibration(calib_file)
+
+        # calibration with mean values of all kitti calibration files
+        calib = Calibration()
+        calib.P = np.array([[719.787081, 0., 608.463003, 44.9538775],
+                      [0., 719.787081, 174.545111, 0.1066855],
+                      [0., 0., 1., 3.0106472e-03]
+                      ])
+        calib.V2C = np.array([
+            [7.49916597e-03, -9.99971248e-01, -8.65110297e-04, -6.71807577e-03],
+            [1.18652889e-02, 9.54520517e-04, -9.99910318e-01, -7.33152811e-02],
+            [9.99882833e-01, 7.49141178e-03, 1.18719929e-02, -2.78557062e-01]
+        ])
+        calib.R0 = np.array([
+            [0.99992475, 0.00975976, -0.00734152],
+            [-0.0097913, 0.99994262, -0.00430371],
+            [0.00729911, 0.0043753, 0.99996319]
+        ])
+
+        calib.c_u = calib.P[0, 2]
+        calib.c_v = calib.P[1, 2]
+        calib.f_u = calib.P[0, 0]
+        calib.f_v = calib.P[1, 1]
+        calib.b_x = calib.P[0, 3] / (-calib.f_u)  # relative
+        calib.b_y = calib.P[1, 3] / (-calib.f_v)
+
+        return calib
 
 
 class Lyft:
@@ -71,13 +97,16 @@ class Audi:
         lidar_file = os.path.join(self.lidar_path, self.files_list[idx])
         assert os.path.exists(lidar_file)
         lidar_pc_raw = np.load(lidar_file)
-        return lidar_pc_raw
+        lidar_pc = np.zeros([lidar_pc_raw['points'].shape[0], 4])
+        lidar_pc[:, :3] = lidar_pc_raw['points']
+        lidar_pc[:, 3] = lidar_pc_raw['reflectance']
+        return lidar_pc
 
     def get_image(self, idx):
         img_file = os.path.join(self.image_path, self.files_list[idx].replace('.npz', '.png').replace('lidar', 'camera'))
         print("IMG: ", img_file)
         assert os.path.exists(img_file)
-        return cv2.cvtColor(cv2.imread(img_file), cv2.COLOR_BGR2RGB)
+        return Image.open(img_file).convert("L")
 
     def get_calib(self, idx):
         calib_file = self.calib_path
